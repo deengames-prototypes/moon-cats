5d06210be988cf5e21a6cee4931e1f19b50f58df
nightblade9 <nightbladecodes@gmail.com>
2020-11-02 22:44:02 -0500

    Fix border walls so they don't overlap

--- source/Scenes/CoreGame/CoreGameScene.gd
+++ source/Scenes/CoreGame/CoreGameScene.gd
@@ -3,6 +3,9 @@ extends Node2D
 const Wall = preload("res://Scenes/CoreGame/Wall.tscn")
 
 func _ready():
+	_create_border_walls()
+
+func _create_border_walls():
 	var w1 = Wall.instance()
 	add_child(w1)
 	w1.resize(1280, 32)
@@ -14,9 +17,10 @@ func _ready():
 	
 	var w3 = Wall.instance()
 	add_child(w3)
-	w3.resize(32, 720)
+	w3.resize(32, 720 - 64)
+	w3.position = Vector2(0, 32)
 	
 	var w4 = Wall.instance()
 	add_child(w4)
-	w4.resize(32, 720)
-	w4.position = Vector2(1280 - 32, 0)
+	w4.resize(32, 720 - 64)
+	w4.position = Vector2(1280 - 32, 32)
