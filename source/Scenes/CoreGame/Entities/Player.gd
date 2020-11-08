b99ffc5c30713f408581cbf9874f99172d321329
nightblade9 <nightbladecodes@gmail.com>
2020-11-08 15:37:26 -0500

    Player rate-limited shots. Also, show bar.

--- source/Scenes/CoreGame/Entities/Player.gd
+++ source/Scenes/CoreGame/Entities/Player.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 const Bullet = preload("res://Scenes/CoreGame/Entities/Bullet.tscn")
 
 const _SPEED:int = 100
+const _SECONDS_BETWEEN_SHOTS:float = 0.3
 
 var _facing:String = "right"
+var _last_shot_time = 0
 
 var _facing_vectors = {
 	"right": Vector2(1, 0),
@@ -14,6 +16,7 @@ var _facing_vectors = {
 }
 
 func _ready():
+	$ReloadBar.max_value = _SECONDS_BETWEEN_SHOTS
 	add_to_group("Player")
 
 func _physics_process(delta):
@@ -33,12 +36,21 @@ func _physics_process(delta):
 		_facing = "right"
 	
 	self.move_and_slide(velocity.normalized() * _SPEED)
-	Globals.is_player_moving = velocity != Vector2.ZERO
+	if velocity != Vector2.ZERO:
+		Globals.is_player_moving = true
+		Globals.total_movement_time += delta
+	else:
+		Globals.is_player_moving = false
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	var now = Globals.total_movement_time
+	var time_since_last_shot = now - _last_shot_time
+	$ReloadBar.value = time_since_last_shot
+	
+	if Input.is_action_pressed("ui_accept") and time_since_last_shot >= _SECONDS_BETWEEN_SHOTS:
 		if velocity == Vector2.ZERO:
 			velocity = _facing_vectors[_facing]
 		self._fire(velocity)
+		self._last_shot_time = now
 	
 func _fire(velocity:Vector2):
 	var instance = Bullet.instance()
--- source/Scripts/Globals.gd
+++ source/Scripts/Globals.gd
@@ -1,3 +1,5 @@
 extends Node
 
+# fractional seconds. total movement time since the game started.
+var total_movement_time:float = 0
 var is_player_moving:bool = false
