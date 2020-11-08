2b279d1051b5aa92b1d7ee71fbda88f89812e87f
nightblade9 <nightbladecodes@gmail.com>
2020-11-08 15:25:24 -0500

    Walls center around origin, can rotate!

--- source/Scenes/CoreGame/Arenas/Wall.gd
+++ source/Scenes/CoreGame/Arenas/Wall.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 export var width:int = 0 setget set_width, get_width
 export var height:int = 0 setget set_height, get_height
 export var health:int = 0 setget set_health, get_health
+export var rotation_speed:int = 0
 
 var _width:int
 var _height:int 
@@ -38,11 +39,19 @@ func on_shot():
 		set_health(_health - 1)
 		if _health <= 0:
 			queue_free()
-	
+
+func _process(delta):
+	if not Engine.editor_hint and rotation_speed > 0:
+		var amount = delta * rotation_speed
+		if Globals.is_player_moving:
+			amount *= 10
+		self.rotation_degrees += amount
+
 func _resize_me() -> void:
 	if $ColorRect != null:
-		$ColorRect.margin_right = _width
-		$ColorRect.margin_bottom = _height
+		$ColorRect.margin_left  = -_width / 2
+		$ColorRect.margin_right = _width / 2
+		$ColorRect.margin_top = -_height / 2
+		$ColorRect.margin_bottom = _height / 2
 	if $CollisionShape2D != null:
 		$CollisionShape2D.shape.extents = Vector2(_width / 2, _height / 2)
-		$CollisionShape2D.position = Vector2(_width / 2, _height / 2) 
