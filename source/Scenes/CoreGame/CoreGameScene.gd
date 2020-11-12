939e3386e3d06126ed8c1361942840a9228ab29d
nightblade9 <nightbladecodes@gmail.com>
2020-11-11 19:03:24 -0500

    Boost display/selection works! Now with health boost.

--- source/Scenes/CoreGame/CoreGameScene.gd
+++ source/Scenes/CoreGame/CoreGameScene.gd
@@ -4,10 +4,15 @@ onready var _arena:Node2D = $Arena
 onready var _blackout:ColorRect = $Blackout
 onready var _boosts:Control = $UI/Boosts
 onready var _start_wave_button:Button = $UI/StartWaveButton
+onready var _boost_buttons = [
+	$UI/Boosts/HBoxContainer/Boost1,
+	$UI/Boosts/HBoxContainer/Boost2,
+	$UI/Boosts/HBoxContainer/Boost3,
+]
 
 func _on_Arena_wave_ready():
-	_boosts.visible = true
-
+	_show_boosts()
+	
 func _on_Arena_waves_complete():
 	print("YOU WIN!!")
 	$Blackout.color.a = 1
@@ -29,3 +34,16 @@ func _on_picked_boost(which:int) -> void:
 	print("Picked boost " + str(which))
 	_boosts.visible = false
 	_start_wave_button.visible = true
+
+func _show_boosts():
+	var all_boosts = ["Damage", "FireRate", "MoveSpeed", "ShotSpeed", "Health"]
+	all_boosts.shuffle() # randomly assign three to buttons
+
+	for i in range(len(_boost_buttons)):
+		var button:Button = _boost_buttons[i]
+		var boost_name = all_boosts[i]
+		button.icon = load("res://Assets/Images/UI/Boost-" + boost_name + ".png")
+		button.text = boost_name
+
+	_boosts.visible = true
+	
